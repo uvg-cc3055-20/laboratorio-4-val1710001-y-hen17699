8a180f91ccabe10247607a8cb096f2300ae3f6f7
FernandoHen <11hengsteber@gmail.com>
2018-02-10 15:40:50 -0600

    Background

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+    public float ScrollingSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.down * ScrollingSpeed * Time.deltaTime);
+
+        if (transform.position.y <= -8.08) 
+        {
+            transform.position = new Vector3(0f, transform.position.x, transform.position.z);
+
+        }
+
+
+    }
+}
