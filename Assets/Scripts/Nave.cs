ba9aa11794517a2d0eaad1ea225823f61e7b8a8e
FernandoHen <11hengsteber@gmail.com>
2018-02-10 14:29:18 -0600

    Menu , Acelerometro

---
+++ Assets/Scripts/Nave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nave : MonoBehaviour {
+
+    Rigidbody2D rb;
+    private float speed = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float movX = Input.acceleration.x; rb.transform.Translate(Vector2.right * speed * movX * Time.deltaTime);
+
+    }
+}
